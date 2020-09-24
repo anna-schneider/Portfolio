d1bb8c9798d8395e366e5d24804e26f48f495008
Anna Schneider <anna.schneiderm@gmail.com>
2020-09-24 17:29:45 -0400

    Header component with animation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from "react"
+
 import "./App.css"
+import Header from "./components/header/Header"
 
 function App() {
 	return (
 		<div className="App">
-			<header>Anna Schneider</header>
+			<Header />
+			{/* <div className="profile-contact-container">
+				<div className="profile-container">
+					<h3 className="profile">PROFILE</h3>
+					<p>
+						I’m a software engineer with a background in managing complex,
+						multi-unit hospitality ventures. My history in the field of training
+						has taught me to approach problem solving with patience and
+						flexibility. As an experienced director of property openings and
+						transitions, I deliver results on budget and on time. I bring with
+						me a consistent track record of delivering frameworks of integrity
+						and partnership.
+					</p>
+				</div>
+				<div className="contact-container">
+					<h3 className="contact">CONTACT</h3>
+				</div>
+			</div> */}
 		</div>
 	)
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+import "./Header.css"
+
+export default function Header() {
+	return (
+		<>
+			<div className="header">
+				<header className="name typewriter-anime">ANNA SCHNEIDER</header>
+				<h2 className="title">Software Engineer</h2>
+			</div>
+			{/* <p className="name typewriter-anime">My name, my title</p> */}
+		</>
+	)
+}
