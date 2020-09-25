300fde3b85307c1f74d5b8852b5abab5096b8fa0
Anna Schneider <anna.schneiderm@gmail.com>
2020-09-24 22:33:30 -0400

    landing styled

--- src/App.js
+++ src/App.js
@@ -6,22 +6,33 @@ import Header from "./components/header/Header"
 function App() {
 	return (
 		<div className="App">
-			<Header />
-			<div className="profile-contact-container">
-				<div className="profile-container">
-					<h3 className="profile">PROFILE</h3>
-					<p>
-						I’m a software engineer with a background in managing complex,
-						multi-unit hospitality ventures. My history in the field of training
-						has taught me to approach problem solving with patience and
-						flexibility. As an experienced director of property openings and
-						transitions, I deliver results on budget and on time. I bring with
-						me a consistent track record of delivering frameworks of integrity
-						and partnership.
-					</p>
+			<div className="landing">
+				<Header className={"top-level-container"} />
+				<div className="profile-contact-container top-level-container">
+					<div className="profile-container">
+						<h3 className="profile">PROFILE</h3>
+						<p className="brand">
+							I’m a software engineer with a background in managing complex,
+							multi-unit hospitality ventures. My history in the field of
+							training has taught me to approach problem solving with patience
+							and flexibility. As an experienced director of property openings
+							and transitions, I deliver results on budget and on time. I bring
+							with me a consistent track record of delivering frameworks of
+							integrity and partnership.
+						</p>
+					</div>
+					<div className="contact-container">
+						<h3 className="contact">CONTACT</h3>
+						<div className="git"></div>
+					</div>
 				</div>
-				<div className="contact-container">
-					<h3 className="contact">CONTACT</h3>
+				{/* <div className="triangle">
+					<h2 className="portfolio">Portfolio</h2>
+				</div> */}
+				<div className="top-level-container">
+					<div className="triangle2">
+						<h2 className="portfolio">Portfolio</h2>
+					</div>
 				</div>
 			</div>
 		</div>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,10 +2,10 @@ import React from "react"
 
 import "./Header.css"
 
-export default function Header() {
+export default function Header(props) {
 	return (
 		<>
-			<div className="header">
+			<div className={`${props.className} header`}>
 				<header className="name typewriter-anime">ANNA SCHNEIDER</header>
 				<h2 className="title typewriter-anime2">Full Stack Developer</h2>
 			</div>
